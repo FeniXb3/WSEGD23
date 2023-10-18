5acc56db4d6062b24cda8eba06c86572519e74a8
Konrad Gadzina <konrad@enklawa.blog>
2023-10-18 10:26:19 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Player 1, provide sign:");
+string firstSign = Console.ReadLine();
+Console.WriteLine(firstSign);
+Console.WriteLine("Player 2, provide sign:");
+string secondSign = Console.ReadLine();
+Console.WriteLine(secondSign);
+
+if (firstSign == secondSign)
+{
+    Console.WriteLine("draw");
+}
