5f85bd0cea382f9df577f21d37bb3f2e67738e48
Konrad Gadzina <konrad@enklawa.blog>
2023-10-25 11:13:05 +0200

    Handle wrong sign for first player

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+					
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Player 1, provide sign:");
 string firstSign = Console.ReadLine();
-Console.WriteLine(firstSign);
+
+//if (!(firstSign == "rock" || firstSign == "paper" || firstSign == "scissors"))
+while (firstSign != "rock" && firstSign != "paper" && firstSign != "scissors")
+{
+	Console.WriteLine("wrong sign");
+	firstSign = Console.ReadLine();
+}
+
 Console.WriteLine("Player 2, provide sign:");
 string secondSign = Console.ReadLine();
-Console.WriteLine(secondSign);
 
 if (firstSign == secondSign)
 {
