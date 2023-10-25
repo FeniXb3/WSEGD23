34ae66789d807fc0adf3b98b4d54ffe87243da15
Konrad Gadzina <konrad@enklawa.blog>
2023-10-25 11:48:07 +0200

    Handle quiting (kinda)

--- Program.cs
+++ Program.cs
@@ -1,36 +1,48 @@
 ﻿using System;
-					
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Player 1, provide sign:");
-string firstSign = Console.ReadLine();
 
-//if (!(firstSign == "rock" || firstSign == "paper" || firstSign == "scissors"))
-while (firstSign != "rock" && firstSign != "paper" && firstSign != "scissors")
+while(true)
 {
-	Console.WriteLine("wrong sign");
-	firstSign = Console.ReadLine();
-}
+	Console.WriteLine("Player 1, provide sign:");
+	string firstSign = Console.ReadLine();
 
-Console.WriteLine("Player 2, provide sign:");
-string secondSign = Console.ReadLine();
+	if (firstSign == "quit")
+	{
+		break;
+	}
+	
+	//if (!(firstSign == "rock" || firstSign == "paper" || firstSign == "scissors"))
+	while (firstSign != "rock" && firstSign != "paper" && firstSign != "scissors")
+	{
+		Console.WriteLine("wrong sign");
+		firstSign = Console.ReadLine();
+		
+		if (firstSign == "quit")
+		{
+			break; // not working as expected - FIX IT
+		}
+	}
 
-while (secondSign != "rock" && secondSign != "paper" && firstSign != "scissors")
-{
-    Console.WriteLine("wrong sign");
-    secondSign = Console.ReadLine();
-}
+	Console.WriteLine("Player 2, provide sign:");
+	string secondSign = Console.ReadLine();
 
-if (firstSign == secondSign)
-{
-    Console.WriteLine("draw");
-}
-else if ((firstSign == "paper" && secondSign == "rock") ||
-    (firstSign == "rock" && secondSign == "scissors") ||
-    (firstSign == "scissors" && secondSign == "paper"))
-{
-    Console.WriteLine("Player 1 won");
-}
-else
-{
-    Console.WriteLine("Player 2 won");
+	while (secondSign != "rock" && secondSign != "paper" && firstSign != "scissors")
+	{
+		Console.WriteLine("wrong sign");
+		secondSign = Console.ReadLine();
+	}
+
+	if (firstSign == secondSign)
+	{
+		Console.WriteLine("draw");
+	}
+	else if ((firstSign == "paper" && secondSign == "rock") ||
+		(firstSign == "rock" && secondSign == "scissors") ||
+		(firstSign == "scissors" && secondSign == "paper"))
+	{
+		Console.WriteLine("Player 1 won");
+	}
+	else
+	{
+		Console.WriteLine("Player 2 won");
+	}
 }
