5006e366f50d8f24705ae12f0ac6df47cb981445
Konrad Gadzina <konrad@makegames.today>
2023-12-20 11:51:35 +0100

    Add points

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
 ﻿string[] availableSigns = {"rock", "paper", "scissors"};
 
+int firstPlayerScore = 0;
+int secondPlayerScore = 0;
+
 while(true)
 {
 	Console.WriteLine("Player 1, provide sign:");
-	string? firstSign = Console.ReadLine().ToLower().Trim();
+	string? firstSign = Console.ReadLine()?.ToLower().Trim();
 	
 	while (!availableSigns.Contains(firstSign) && firstSign != "quit")
 	{
 		Console.WriteLine("wrong sign");
-		firstSign = Console.ReadLine().ToLower().Trim();
+		firstSign = Console.ReadLine()?.ToLower().Trim();
 	}
 
 	if (firstSign == "quit")
@@ -17,12 +20,12 @@ while(true)
 	}
 
 	Console.WriteLine("Player 2, provide sign:");
-	string? secondSign = Console.ReadLine().ToLower().Trim();
+	string? secondSign = Console.ReadLine()?.ToLower().Trim();
 
 	while (!availableSigns.Contains(secondSign) && secondSign != "quit")
 	{
 		Console.WriteLine("wrong sign");
-		secondSign = Console.ReadLine().ToLower().Trim();
+		secondSign = Console.ReadLine()?.ToLower().Trim();
 	}
 
 	if (secondSign == "quit")
@@ -41,9 +44,15 @@ while(true)
 	else if(firstSign == secondSignWinningSign)
 	{
 		Console.WriteLine("Player 1 won");
+		// firstPlayerScore = secondPlayerScore + 1;
+		firstPlayerScore += 1;
+		// firstPlayerScore++;
 	}
 	else
 	{
 		Console.WriteLine("Player 2 won");
+		secondPlayerScore += 1;
 	}
+
+	Console.WriteLine($"[Player 1] {firstPlayerScore} : {secondPlayerScore} [Player 2]");
 }
